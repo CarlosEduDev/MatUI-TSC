4f397f2c45d7c470f52ae1088517cf1ea7276f59
Carlos Eduardo Evangelista <127114935+CarlosEduDev@users.noreply.github.com>
2023-11-20 16:38:07 -0300

    fix: UseDebounce.ts

--- material-ui/src/shared/hooks/UseDebounce.ts
+++ material-ui/src/shared/hooks/UseDebounce.ts
@@ -20,26 +20,3 @@ export const useDebounce = (delay = 300, notDelayInFirstTime = true) => {
 
   return { debounce };
 };
-
-// import {useCallback, useRef} from 'react'
-
-// export const useDebounce = (delay = 300, notDelayFirtTime = true) => {
-// const debouncing = useRef<NodeJS.Timeout>();
-// const isFirstTime = useRef(notDelayFirtTime);
-
-//    const debounce = useCallback((func: () => void) => {
-
-//       if(isFirstTime.current){
-//          isFirstTime.current = false;
-//          func();
-
-//       }else{
-//          if(debouncing.current){
-//             clearTimeout(debouncing.current);
-//          }
-//          debouncing.current = setTimeout(() => func(), delay);
-//       }
-//    }, [delay]);
-
-//    return{debounce};
-// };
